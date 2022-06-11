877db4072258666fd1ab7196daa2b17c7dbc2e20
diamond-nicholas <diamondnicholas154@gmail.com>
2022-06-11 15:21:06 +0100

    add logic to read hands on live videocam, use mediapipe to draw on hands

--- htrack.py
+++ htrack.py
@@ -4,8 +4,19 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     isTrue, frame = cap.read()
+    imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(frame, handLms)
     if isTrue:
         cv2.imshow('Video', frame)
         if cv2.waitKey(20) & 0xFF==ord('d'):

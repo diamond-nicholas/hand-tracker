9c8564348b10527dcb5f3084d7795eac46e7f512
diamond-nicholas <diamondnicholas154@gmail.com>
2022-06-11 18:43:34 +0100

    update logic and create module

--- htrack.py
+++ htrack.py
@@ -7,6 +7,8 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
 
 while True:
     isTrue, frame = cap.read()
@@ -16,7 +18,21 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(frame, handLms)
+            for id, lm in enumerate(handLms.landmark):
+                print(id, lm)
+                h,w,c = frame.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 8:
+                    cv2.circle(frame, (cx, cy), 15, (255,0,255), cv2.FILLED)
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(frame, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 255), 3)
+
     if isTrue:
         cv2.imshow('Video', frame)
         if cv2.waitKey(20) & 0xFF==ord('d'):
